7bcfa0ffd1277e18f91bdb7ae694042b9b42e4c5
CoEFew <surasak.nk24@gmail.com>
2022-04-20 16:10:17 +0700

    npm run build

---
+++ build/precache-manifest.00cb6542db12ddd6d50a286bfcff049c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "890ecc1a304793ce9a03",
+    "url": "/static/css/main.610ca8ef.chunk.css"
+  },
+  {
+    "revision": "890ecc1a304793ce9a03",
+    "url": "/static/js/main.55b141ec.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bccc50be985d61131c60",
+    "url": "/static/css/2.b94b79ea.chunk.css"
+  },
+  {
+    "revision": "bccc50be985d61131c60",
+    "url": "/static/js/2.fb34f85e.chunk.js"
+  },
+  {
+    "revision": "a9733b71ae7744545cd57457b5a5c121",
+    "url": "/index.html"
+  }
+];
